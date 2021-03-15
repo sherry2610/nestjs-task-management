4def54b8d4e876b7f2fc7cfa55ce67fc3270f8dd
sherry2610 <shaharyarkhalid2610@gmail.com>
2021-03-15 19:07:17 +0500

    feat: create model schema for task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Tasks } from './tasks.model';
 
 @Injectable()
 export class TasksService {
-    private tasks = [];
+    private tasks: Tasks[] = [];
 
-    getAllTask (){
+    getAllTask (): Tasks[] {
         return this.tasks;
     }
 }
